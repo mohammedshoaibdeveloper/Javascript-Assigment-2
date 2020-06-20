77022545053b23126792826cb6d08c889dea8dfd
Mohammed Shoaib <shoaibbilal101@gmail.com>
2020-06-20 22:00:42 +0500

    ch 34 complete

--- app.js
+++ app.js
@@ -286,10 +286,164 @@
 // between 1 and 10. If the user input equals the secret
 // number, congratulate the user.
 
-var num = Math.random() * (10 - 1) + 1;
-user = prompt("Enter a number")
-if (num == user) {
-    document.write("Congrats")
-} else {
-    document.write("Try Again")
-}
+// var num = Math.random() * (10 - 1) + 1;
+// user = prompt("Enter a number")
+// if (num == user) {
+//     document.write("Congrats")
+// } else {
+//     document.write("Try Again")
+// }
+
+// ch 31-34
+// DATE METHODS
+// 1. Write a program that displays current date and time in
+// your browser.'
+
+// var a = new Date()
+// document.write(a)
+
+// 2. Write a program that alerts the current month in words.
+// For example December.
+
+// const monthNames = ["January", "February", "March", "April", "May", "June",
+//     "July", "August", "September", "October", "November", "December"
+// ];
+// var a = new Date()
+// document.write(monthNames[a.getMonth()])
+
+// 3. Write a program that alerts the first 3 letters of the current
+// day, for example if today is Sunday then alert will show
+// Sun.
+
+// dayname = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'sat']
+// var a = new Date()
+// var day = dayname[a.getDay()]
+// document.write(day.slice(0, 3))
+
+// 4. Write a program that displays a message “It’s Fun day” if
+// its Saturday or Sunday today.
+
+// dayname = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'sat']
+// var a = new Date()
+// var day = dayname[a.getDay()]
+// if (day == dayname[6] || day == dayname[0]) {
+
+
+//     document.write("Its funday")
+// }
+
+// 5. Write a program that shows the message “First fifteen
+// days of the month” if the date is less than 16th of the month
+// else shows “Last days of the month”.
+
+// var a = new Date()
+// var day = a.getDate()
+// if (day < 16) {
+//     var msg = "First fifteen days of the month";
+// } else {
+//     document.write("Last days of the month")
+
+// }
+
+// 6. Write a program that determines the minutes since
+// midnight, Jan. 1, 1970 and assigns it to a variable that
+// hasn't been declared beforehand. Use any variable you like
+// to represent the Date object.
+
+// var date = new Date()
+// var milSecond = date.getTime()
+// var min = (milSecond / (1000 * 60)).toFixed(1)
+// document.write("Current Date is ", date, "<br>")
+// document.write("Millisecond from january 1 ,1970 ", milSecond, "<br>")
+// document.write("Minutes from january 1 ,1970 ", min, "<br>")
+
+// 7. Write a program that tests whether it's before noon and
+// alert “Its AM” else “its PM”.
+
+// var date = new Date()
+// var hour = date.getHours()
+// if (hour < 12) {
+//     document.write("Its am")
+// } else {
+//     document.write("Its pm")
+// }
+
+// 8. Write a program that creates a Date object for the last day
+// of the last month of 2020 and assigns it to variable named
+// laterDate.
+
+// var date = new Date("Dec 31,2020")
+// document.write(date)
+// 9. Create a date object of the starting date of this Ramadan
+// and alert the number of days past since 1 st Ramadan ?
+//     Note : 1 st Ramadan was on June 18, 2015
+
+// var ramzanFirst = new Date("April 25,2020").getTime()
+// var currentDate = new Date().getTime()
+// var diff = currentDate - ramzanFirst
+// var res = diff / (1000 * 60 * 60 * 24)
+// document.write(Math.floor(res), " Days passed since 1st ramzan")
+
+// 10. Write a program that displays in your browser the
+// seconds that elapsed between the reference date and the
+// beginning of 2015.
+
+// var Reference = new Date("2020").getTime()
+// document.write(Reference, " Seconds had passed since begning of 2020")
+
+// 11. Create a Date object for the current date and time.
+// Extract the hours, reset the date object an hour ahead and
+// finally display the date object in your browser.
+// var a = new Date()
+// document.write("Current date and time is ", a, "<br>")
+// var b = a.getHours()
+// var c = new Date()
+// c.setHours(b - 1)
+// document.write("Time 1 hour a ago", c)
+
+// 12. Write a program that creates a date object and show the
+// date in an alert box that is reset to 100 years back?
+// var a = new Date();
+// document.write("Current date is ", a, "<br>");
+// var b = a.getFullYear();
+// var c = new Date();
+// c.setFullYear(b - 100);
+// document.write(c)
+
+// 13. Write a program to ask the user about his age. Calculate
+// and show his birth year in your browser
+
+// var age = 22;
+// var currentYear = 2020;
+// document.write("Your birth year is ", currentYear - age)
+
+// 14. Write a program to generate your K - Electric bill in your
+// browser.All the amounts should be rounded off to 2
+// decimal places.Display the following fields:
+//     a.Customer Name
+// b.Current Month
+// c.Number of units
+// d.Charges per unit
+// e.Net Amount Payable(within Due Date)
+// f.Late Payment Surcharge
+// g.Gross Amount Payable(after Due Date)
+// Where,
+
+document.write("<h1>", "K-Electric Bill", "</h1>", "<br>");
+var customerName = "shoaib";
+var curentMonth = "june";
+var NumberOfUnit = 200;
+var perUnit = 16;
+var amount = 0;
+var lateamount = 200;
+
+document.write("CustomerName : ", customerName, "<br>")
+document.write("Month : ", curentMonth, "<br>")
+document.write("Number of Units : ", NumberOfUnit, "<br>")
+document.write("Charge per unit : ", perUnit, "<br><br><br>")
+
+
+
+document.write("Net Amount payable with due date : ", 200 * 16, "<br>")
+document.write("surcharge : ", lateamount, "<br>")
+document.write("After due date : ", (200 * 16) + 200, "<br>")
